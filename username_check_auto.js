0690335f13188e8d71363bb9080018aeffcc829d
Dmitry Kresin <ardas@33360.no-reply.drupal.org>
2009-01-22 14:10:03 +0000

    *** empty log message ***

--- username_check_auto.js
+++ username_check_auto.js
@@ -1,67 +1,73 @@
-var usernameCheckTimer;
-var usernameCheckUsername = '';
+// $Id$
 
-if (Drupal.jsEnabled) {
-  $(document).ready(function() {
-    var usernamePos = $('#username-check-wrapper input').position();
-    var usernameWidth = $('#username-check-wrapper input').width();
-    $('#username-check-informer').css({left: (usernamePos.left+usernameWidth+10)+'px', top: (usernamePos.top)+'px'}).show();
-    
-    $('#username-check-wrapper input').
-      keyup(function() {
-        if($('#username-check-wrapper input').val() != usernameCheckUsername) {
-          clearTimeout(usernameCheckTimer);
-          usernameCheckTimer = setTimeout('usernameCheck()', Drupal.settings.usernameCheck.delay*1000);
+Drupal.behaviors.usernameCheckAutoBehavior = function (context) {
+  $('#username-check-wrapper input:not(.username-check-behavior-processed)', context)
+    .addClass('username-check-behavior-processed')
+    .each(function () {
+      var loginField = $(this); 
+      
+      Drupal.usernameCheckUsername = '';
+      Drupal.usernameCheckUsernamePos = loginField.position();
+      Drupal.usernameCheckUsernameWidth = loginField.width();
+      $('#username-check-informer')
+        .css({left: (Drupal.usernameCheckUsernamePos.left+Drupal.usernameCheckUsernameWidth+10)+'px', top: (Drupal.usernameCheckUsernamePos.top)+'px'})
+        .show(); 
+      
+      loginField
+        .keyup(function () {
+          if(loginField.val() != Drupal.usernameCheckUsername) {
+            clearTimeout(Drupal.usernameCheckTimer);
+            Drupal.usernameCheckTimer = setTimeout(function () {usernameCheck(loginField)}, Drupal.settings.usernameCheck.delay*1000);
           
-          if(!$("#username-check-informer").hasClass('username-check-informer-progress')) {
-            $("#username-check-informer").
-              removeClass('username-check-informer-accepted').
-              removeClass('username-check-informer-rejected');
-          }
+            if(!$("#username-check-informer").hasClass('username-check-informer-progress')) {
+              $("#username-check-informer")
+                .removeClass('username-check-informer-accepted')
+                .removeClass('username-check-informer-rejected');
+            }
               
-          $("#username-check-message").
-            hide();
-        }
-      }).
-      blur(function() {
-        if($('#username-check-wrapper input').val() != usernameCheckUsername) {
-          usernameCheck();
-        }
-      });
-  });
+            $("#username-check-message").hide();
+          }
+        })
+        .blur(function () {
+          if(loginField.val() != Drupal.usernameCheckUsername) {
+            usernameCheck(loginField);
+          }
+        });    
+    });
 }
 
-function usernameCheck() {
-  clearTimeout(usernameCheckTimer);
-  usernameCheckUsername = $('#username-check-wrapper input').val();
-
+function usernameCheck(loginField) {
+  clearTimeout(Drupal.usernameCheckTimer);
+  Drupal.usernameCheckUsername = loginField.val();
+  
   $.ajax({
     url: Drupal.settings.usernameCheck.ajaxUrl,
-    data: {username: usernameCheckUsername},
+    data: {username: loginField.val()},
     dataType: 'json',
     beforeSend: function() {
-      $("#username-check-informer").
-        removeClass('username-check-informer-accepted').
-        removeClass('username-check-informer-rejected').
-        addClass('username-check-informer-progress');
+      $("#username-check-informer")
+        .removeClass('username-check-informer-accepted')
+        .removeClass('username-check-informer-rejected')
+        .addClass('username-check-informer-progress');
     },
     success: function(ret){
       if(ret['allowed']){
-        $("#username-check-informer").
-          removeClass('username-check-informer-progress').
-          addClass('username-check-informer-accepted');
-        $("#username-check-wrapper input").
-          removeClass('error');
+        $("#username-check-informer")
+          .removeClass('username-check-informer-progress')
+          .addClass('username-check-informer-accepted');
+        
+        loginField
+          .removeClass('error');
       }
       else {
-        $("#username-check-informer").
-          removeClass('username-check-informer-progress').
-          addClass('username-check-informer-rejected');
+        $("#username-check-informer")
+          .removeClass('username-check-informer-progress')
+          .addClass('username-check-informer-rejected');
         
-        $("#username-check-message").
-            addClass('username-check-message-rejected').
-            html(ret['msg']).
-            show();
+        $("#username-check-message")
+          .addClass('username-check-message-rejected')
+          .html(ret['msg'])
+          .show();
       }
     }
    });
--- username_check_manual.js
+++ username_check_manual.js
@@ -1,39 +1,41 @@
-if (Drupal.jsEnabled) {
-  $(document).ready(function() {
-    var username = '';
-    $('#edit-username-check-button').show();
-    $('#edit-username-check-button').click(function() {
-      username = $("#username-check-wrapper input").val();
+// $Id$
 
+Drupal.behaviors.usernameCheckManualBehavior = function (context) {
+  $('#edit-username-check-button:not(.username-check-behavior-processed)', context)
+    .addClass('username-check-behavior-processed')
+    .show()
+    .click(function () {
+      Drupal.usernameCheckUsername = $("#username-check-wrapper input").val();
+      
       $.ajax({
         url: Drupal.settings.usernameCheck.ajaxUrl,
-        data: {username: username},
+        data: {username: Drupal.usernameCheckUsername},
         dataType: 'json',
         beforeSend: function() {
-          $("#username-check-message").
-            removeClass('username-check-message-accepted').
-            removeClass('username-check-message-rejected').
-            addClass('username-check-message-progress').
-            html(Drupal.settings.usernameCheck.msgWait).
-            show();
+          $("#username-check-message")
+            .removeClass('username-check-message-accepted')
+            .removeClass('username-check-message-rejected')
+            .addClass('username-check-message-progress')
+            .html(Drupal.settings.usernameCheck.msgWait)
+            .show();
         },
         success: function(ret){
           if(ret['allowed']){
-            $("#username-check-message").
-              removeClass('username-check-message-progress').
-              addClass('username-check-message-accepted');
+            $("#username-check-message")
+              .removeClass('username-check-message-progress')
+              .addClass('username-check-message-accepted');
           }
           else {
-            $("#username-check-message").
-              removeClass('username-check-message-progress').
-              addClass('username-check-message-rejected');
+            $("#username-check-message")
+              .removeClass('username-check-message-progress')
+              .addClass('username-check-message-rejected');
           }
-          $("#username-check-message").
-            removeClass('username-check-message-progress').
-            html(ret['msg']);
+          $("#username-check-message")
+            .removeClass('username-check-message-progress')
+            .html(ret['msg']);
         }
       });
+      
       return false;
     });
-  });
 }
